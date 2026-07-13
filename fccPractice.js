b9c4c79527a24e1ccac3a4d658278f5f35476793
Harry <harrylibrando06@gmail.com>
2026-07-13 17:01:59 +0800

    added widelife tracker from fcc

--- fccPractice.js
+++ fccPractice.js
@@ -846,3 +846,62 @@ const numForObj = 42;
 const objNum = Object(numForObj); // create a wrapper for the number
 console.log(objNum); // 42
 console.log(typeof objNum); // 'object'
+
+const tiger = {
+  species: "Tiger",
+  age: 5,
+  isEndangered: true,
+};
+
+const elephant = {
+  species: "Elephant",
+  age: 10,
+  isEndangered: true,
+};
+
+const getSpecies = (animal) => {
+  return animal.species;
+};
+
+console.log(getSpecies(tiger));
+
+const getAge = (animal) => {
+  return animal.age;
+};
+
+console.log(getAge(tiger));
+
+const addHabitat = (animal, habitat) => {
+  animal.habitat = habitat;
+  return animal;
+};
+
+console.log(addHabitat(tiger, "Rainforest"));
+
+const updateAge = (animal, newAge) => {
+  animal.age = newAge;
+  return animal;
+};
+
+console.log(updateAge(elephant, 12));
+
+const removeEndangeredStatus = (animal) => {
+  delete animal.isEndangered;
+  return animal;
+};
+
+console.log(removeEndangeredStatus(tiger));
+
+const hasHabitat = (animal) => {
+  return animal.hasOwnProperty("habitat");
+};
+
+console.log(hasHabitat(tiger));
+console.log(hasHabitat(elephant));
+
+const getProperty = (animal, propertyName) => {
+  return animal[propertyName];
+};
+
+console.log(getProperty(tiger, "species"));
+console.log(getProperty(elephant, "age"));
