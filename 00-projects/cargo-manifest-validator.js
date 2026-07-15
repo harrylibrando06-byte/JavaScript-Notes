86ae37d06f262c78db44e0fb6d62da4a1e158d12
Harry <harrylibrando06@gmail.com>
2026-07-15 17:27:39 +0800

    added more if clauses to validate manifest

--- 00-projects/cargo-manifest-validator.js
+++ 00-projects/cargo-manifest-validator.js
@@ -9,13 +9,69 @@ function normalizeUnits(manifest) {
 
 console.log(normalizeUnits());
 
-const validateManifest = (manifest) => {
+function validateManifest(manifest) {
   let problem = {};
 
   if (!Object.hasOwn(manifest, "containerId")) {
     problem.containerId = "Missing";
-  } else {
+  } else if (
+    manifest.containerId <= 0 ||
+    !Number(manifest.container) ||
+    manifest.containerId === null
+  ) {
     problem.containerId = "Invalid";
+  } else {
+    problem;
   }
-  //repeat this for the other property
-};
+
+  if (!Object.hasOwn(manifest, "destination")) {
+    problem.destination = "Missing";
+  } else if (
+    !typeof manifest.destination === "string" ||
+    manifest.destination === null
+  ) {
+    problem.destination = "Invalid";
+  } else {
+    problem;
+  }
+
+  if (!Object.hasOwn(manifest, "weight")) {
+    problem.weight = "Missing";
+  } else if (
+    !typeof manifest.weight === "number" ||
+    manifest.weight === null ||
+    manifest.weight <= 0
+  ) {
+    problem.weight = "Invalid";
+  } else {
+    problem;
+  }
+
+  if (!Object.hasOwn(manifest, "unit")) {
+    problem.unit = "Missing";
+  } else if (!typeof manifest.unit === "string" || manifest.unit === null) {
+    problem.unit = "Invalid";
+  } else {
+    problem;
+  }
+
+  if (!Object.hasOwn(manifest, "hazmat")) {
+    problem.hazmat = "Missing";
+  } else if (manifest.hazmat === null) {
+    problem.hazmat = "Invalid";
+  } else {
+    problem;
+  }
+
+  return problem;
+}
+
+console.log(
+  validateManifest({
+    containerId: 1,
+    destination: "Santa Cruz",
+    weight: 304,
+    unit: "kg",
+    hazmat: false,
+  }),
+);
