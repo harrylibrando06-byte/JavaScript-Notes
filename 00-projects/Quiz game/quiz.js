e6c2a640edb85c899434ab6003c70daf4aba3fb4
Harry <harrylibrando06@gmail.com>
2026-07-24 18:14:31 +0800

    started with quiz and added 5 object with 4 keys

---
+++ 00-projects/Quiz game/quiz.js
@@ -0,0 +1,38 @@
+const questions = [
+  //
+  {
+    category: "Movies",
+    question: "Who directed Parasite?",
+    choices: ["Park Chan-wook", "Bong Joon-ho", "Kim Ki-duk"],
+    answer: "Bong Joon-ho",
+  },
+  {
+    category: "Technology",
+    question: "What does RAM stand for?",
+    choices: [
+      "Random Access Memory",
+      "Read And Modify",
+      "Rapid Application Module",
+    ],
+    answer: "Random Access Memory",
+  },
+  {
+    category: "Geography",
+    question: "What is the capital of Japan?",
+    choices: ["Seoul", "Beijing", "Tokyo"],
+    answer: "Tokyo",
+  },
+
+  {
+    category: "Science",
+    question: "What is H2O?",
+    choices: ["Water", "Oxygen", "Hydrogen"],
+    answer: "Water",
+  },
+  {
+    category: "History",
+    question: "Who discovered the Philippines?",
+    choices: ["Magellan", "Columbus", "Vespucci"],
+    answer: "Magellan",
+  },
+];
